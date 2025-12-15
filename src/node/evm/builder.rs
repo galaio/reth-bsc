80e60b4ad497e292f2f173e6e40e0e32ec7b900c
galaio <galaio@users.noreply.github.com>
2025-12-15 23:19:47 +0800

    mining: support parallel state root calculation;

--- src/node/evm/builder.rs
+++ src/node/evm/builder.rs
@@ -1,12 +1,16 @@
 use crate::{BscPrimitives, hardforks::BscHardforks, node::{engine_api::payload::BscPayloadTypes, evm::{assembler::{BscBlockAssembler, BscBlockAssemblerInput}, config::{BscBlockExecutionCtx, BscBlockExecutorFactory, BscExecutionSharedCtx}, executor::BscBlockExecutor, factory::BscEvmFactory, pre_execution::{TURN_LENGTH_CACHE, VALIDATOR_CACHE}}}};
+use alloy_primitives::BlockHash;
 use reth_engine_primitives::{BSCEngineMessageError, ConsensusEngineHandle};
+use reth_engine_tree::tree::CustomRequestMessage;
 use reth_evm::execute::{BlockBuilder, BlockBuilderOutcome, BlockExecutionError, ExecutorTx};
 use alloy_evm::eth::receipt_builder::ReceiptBuilder;
 use reth_primitives_traits::{HeaderTy, NodePrimitives, Recovered, RecoveredBlock, SealedHeader, SignerRecoverable, TxTy};
 use reth_provider::StateProvider;
+use reth_trie_parallel::root::ParallelStateRoot;
 use revm::database::{State, states::bundle_state::BundleRetention};
 use alloy_evm::{Evm, block::BlockExecutor};
 use reth_chainspec::{EthChainSpec, EthereumHardforks, Hardforks};
+use tokio::sync::oneshot;
 
 
 /// rewrite BasicBlockBuilder, mainly about the finish() trait.
@@ -113,20 +117,21 @@ where
         // calculate the state root
         let state_root_start = std::time::Instant::now();
         let hashed_state = state.hashed_post_state(&db.bundle_state);
+        let parent_hash = self.parent.hash_slow();
         
         if let Some(engine) = crate::shared::get_engine_handle() {
-            let td = if let Ok(handle) = tokio::runtime::Handle::try_current() {
+            let parallel_state_root_task = if let Ok(handle) = tokio::runtime::Handle::try_current() {
                 let number = self.parent.number;
                 let hash = self.parent.hash_slow();
                 let fut = async {
-                    engine.query_td(number, hash).await
+                    request_parallel_state_root(&engine, parent_hash).await
                 };
                 tokio::task::block_in_place(|| handle.block_on(fut))
             } else {
                 let number = self.parent.number;
                 let hash = self.parent.hash_slow();
                 let fut = async {
-                    engine.query_td(number, hash).await
+                    request_parallel_state_root(&engine, parent_hash).await
                 };
                 match tokio::runtime::Builder::new_current_thread().enable_all().build() {
                     Ok(rt) => {
@@ -137,6 +142,8 @@ where
                     }
                 }
             }.map_err(BlockExecutionError::other)?;
+            parallel_state_root_task.append_state(&hashed_state);
+            let (state_root, trie_updates) = parallel_state_root_task.incremental_root_with_updates()?;
         }
         let (state_root, trie_updates) = state
             .state_root_with_updates(hashed_state.clone())
@@ -207,4 +214,13 @@ where
     fn into_executor(self) -> Self::Executor {
         self.executor
     }
+}
+
+pub async fn request_parallel_state_root(
+    engine: &ConsensusEngineHandle<BscPayloadTypes>,
+    parent_hash: BlockHash,
+) -> Result<ParallelStateRoot, BSCEngineMessageError> {
+    let (tx, rx) = oneshot::channel();
+    let _ = engine.to_engine.send(CustomRequestMessage::RequestParallelStateRoot { parent_hash, tx });
+    rx.await.map_err(BSCEngineMessageError::internal)?.map_err(BSCEngineMessageError::internal)
 }
