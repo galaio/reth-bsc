578fe0687cf4231ced0f31961367b37eddfd17ed
galaio <galaio@users.noreply.github.com>
2025-12-17 15:38:53 +0800

    mining: support parallel state root calculation;

--- src/node/miner/payload.rs
+++ src/node/miner/payload.rs
@@ -172,7 +172,7 @@ where
         let mut db = State::builder().with_database(cached_reads.as_db_mut(state)).with_bundle_update().build();
         
         let mut builder = self.evm_config
-            .builder_for_next_block(
+            .builder_sparse_trie_for_next_block(
                 &mut db,
                 &parent_header,
                 NextBlockEnvAttributes {
